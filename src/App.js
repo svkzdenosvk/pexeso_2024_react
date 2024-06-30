26cffedb4822097c7a22f83211d7766825043746
https://github.com/svkzdenosvk <zdenko.snurik@gmail.com>
2024-07-01 00:34:11 +0200

    styling welcome page before game

--- src/App.js
+++ src/App.js
@@ -13,15 +13,19 @@ const App = () =>{
                        setSeconds={setSeconds} 
                        setIntervalSecond={setIntervalSecond}
                        color={color}/>  */}
-      
-        <div id="levelBtns" /*style={{display}}*/ >
-            <SetLevelBtns /*my_setLevel={my_setLevel}*//>
-        </div>
+         <div className="welcome">
+            <h1> {/*style={{color}}   {h1Context} */}Pexeso </h1>
+          
+            <h3>Vitajte v hre pexeso, pre začatie hry zvoľte náročnosť nižšie </h3>
+
+            <div id="levelBtns" /*style={{display}}*/ >
+              <SetLevelBtns /*my_setLevel={my_setLevel}*//>
+            </div>
+
+         </div>
       </div>
 
-        <h1> {/*style={{color}}  
-        {h1Context} */}
-      </h1>
+       
 
       <div className="column_content" id="content">
         <Game /*level={level} shuffle={shuffle} seconds={seconds} intervalSecond={intervalSecond}*//> 
--- src/components/BeforeGame/SetLevelBtns.js
+++ src/components/BeforeGame/SetLevelBtns.js
@@ -3,12 +3,12 @@ import {Button} from "./Button";
 
 export const SetLevelBtns = () =>{
     return (
-      <div id="levelBtns" >
+      <>
         <Button /*my_setLevel={my_setLevel}*/ text="NORMAL" id="normal"/>
         <Button /*my_setLevel={my_setLevel}*/ text="HARDER" id="harder"/>
         <Button /*my_setLevel={my_setLevel}*/ text="HARDEST" id="hardest"/>
+      </>
 
-      </div>
     );
   }
   
