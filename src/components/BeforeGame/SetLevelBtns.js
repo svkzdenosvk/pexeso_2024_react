5d1b055be64930a47ebefe4a731bd29b6e304611
https://github.com/svkzdenosvk <zdenko.snurik@gmail.com>
2024-06-30 02:07:40 +0200

    styling level buttons

--- src/components/BeforeGame/SetLevelBtns.js
+++ src/components/BeforeGame/SetLevelBtns.js
@@ -4,9 +4,9 @@ import {Button} from "./Button";
 export const SetLevelBtns = () =>{
     return (
       <div id="levelBtns" >
-        <Button/>
-        <Button/>
-        <Button/>
+        <Button /*my_setLevel={my_setLevel}*/ text="NORMAL" id="normal"/>
+        <Button /*my_setLevel={my_setLevel}*/ text="HARDER" id="harder"/>
+        <Button /*my_setLevel={my_setLevel}*/ text="HARDEST" id="hardest"/>
 
       </div>
     );
