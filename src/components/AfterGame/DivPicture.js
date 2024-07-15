f5b7d4ba3cddc97111b7d55fe4cbadfc7b1119e1
https://github.com/svkzdenosvk <zdenko.snurik@gmail.com>
2024-07-15 23:12:41 +0200

    rewrite toggle function and clean a bit code

--- src/components/AfterGame/DivPicture.js
+++ src/components/AfterGame/DivPicture.js
@@ -1,3 +1,5 @@
+import { _fmtMSS, _myToggle } from "./../../_inc/_inc_functions";
+
  //component about one single div > picture
 
 export const DivPicture = (props) =>{
@@ -11,73 +13,68 @@ export const DivPicture = (props) =>{
     document.getElementById("seconds").style.display="none";
   }
 
-  function _fmtMSS(s){return(s-(s%=60))/60+(9<s?':':':0')+s}/*--------------------formate seconds -> time */
-
  // ---------------------------
  // ---------------------------ending fn
  // ---------------------------
 
-  function checkEnd(){/*----------------------------------------------------------check if is end == each picture removed */
-        if(!document.getElementById("row").firstElementChild){/*------------------if all images on page are removed */
+  function checkEnd(){/*------------------------------------------------------------check if is end == each picture removed */
+        if(!document.getElementById("row").firstElementChild){/*--------------------if all images on page are removed */
 
-            stopTimer();/*--------------------------------------------------------stop increment seconds */
-            let endTime=_fmtMSS(props.seconds);/*---------------------------------formating time */
+            stopTimer();/*----------------------------------------------------------stop increment seconds */
+            let endTime=_fmtMSS(props.seconds);/*-----------------------------------formating time */
 
             document.getElementsByTagName("BODY")[0].firstElementChild.classList.add('div_center');/*---------------start ---animation of gratulation text */
-            let timeArr=endTime.split(":");/*--------------------------------------split time string (seconds:minutes) to array for separate minutes and second in gratulation text */
+            let timeArr=endTime.split(":");/*---------------------------------------split time string (seconds:minutes) to array for separate minutes and second in gratulation text */
 
             document.getElementsByTagName("H1")[0].innerHTML = "Gratulácia, vyhrali ste za "+(timeArr[0]==="0"?"":timeArr[0]+"m")+" "+ timeArr[1]+"s";
-            document.getElementsByTagName("H1")[0].classList.add('h1End');/*------------------------------------end ---animation of gratulation text */
+            document.getElementsByTagName("H1")[0].classList.add('h1End');/*---------end ---animation of gratulation text */
         }
-}
+  }
 
  // ---------------------------
  // ---------------------------fn´s to show / hide animate and delete div>imgs
  // ---------------------------
 
-  function _deleteImg(el,checkEndCallBack){/*--------------------------------------------------------partial f. to remove the same showed images*/
-       el.remove();
-       setTimeout(function(){
-          checkEndCallBack();
-       }, 50);
+  function _deleteImg(el,checkEndCallBack){/*-----------------------------------------partial f. to remove the same showed images*/
+    el.remove();
+    setTimeout(function(){
+      checkEndCallBack();
+    }, 50);
   }
 
-  function _animate(element, deleteImgCallBack,checkEndCallBack){/*----------------------------------------------------- animation to rotate -> then delete div>img*/ 
+  function _animate(element, deleteImgCallBack,checkEndCallBack){/*-------------------animation to rotate -> then delete div>img*/ 
 
     element.classList.add("rotate-center");
-    //rewriten as callback f.
-     setTimeout(function(){
+     setTimeout(function(){/*---------------------------------------------------------rewriten as callback f.*/
        deleteImgCallBack(element,checkEndCallBack);
-   }, 210);
+     }, 210);
   }
   
-  function animateAndDelete(first,second){/*-----------------------------------------matched img´s animate->delete*/ 
+  function animateAndDelete(first,second){/*------------------------..-----------------matched img´s animate->delete*/ 
     _animate(first, _deleteImg, checkEnd);
     _animate(second, _deleteImg, checkEnd);
   }
 
-  function _hideImage(elm){/*--------------------------------------------------------partial f. to hide showed image unmatched*/
-    elm.classList.add('mask');/*-----------------------------------------------------hide image below joker´s image*/
-    elm.classList.remove('selected_Div_img');/*--------------------------------------remove specific class for identification*/
+  function showImg(element){
+    _myToggle(element,'mask','selected_Div_img');
   }
 
-  function hideUnMatched(first, second){
-    _hideImage(first);
-    _hideImage(second);
+  function hideUnMatched(first, second){/*----------------------------------------------if shown images do not match -> hide them back  */
+    _myToggle(first, 'selected_Div_img', 'mask');
+    _myToggle(second, 'selected_Div_img', 'mask');
   }
 
  // ---------------------------
  // ---------------------------main fn to compare
  // ---------------------------
 
- function mainFn(element) {/*--------------------------------------------------------the most main function to manage pexeso-code */
-       console.log(element);
-    if(element.classList.contains('mask')){/*----------------------------------------if on image is joker´s image */
+  function mainFn(element) {/*--------------------------------------------------------the most main function to manage pexeso-code */
+
+    if(element.classList.contains('mask')){/*------------------------------------------if on image is joker´s image */
 
       var imgElm = element.firstElementChild;
-      element.classList.remove('mask');/*--------------------------------------------remove joker image */
-      element.classList.add('selected_Div_img');/*-----------------------------------give specific class for identification*/
-
+      showImg(element);/*------------------------------------------------------------remove joker image and show img under */
+      
       if(props.stticSource===""){/*--------------------------------------------------if no image is shown, get attribute from clicked*/
          
          props.setStticSource(imgElm.getAttribute("src"));
@@ -110,8 +107,8 @@ export const DivPicture = (props) =>{
               }, 300);
           }
       }
-   }
-}
+    }
+  }
 
     return (
         <div onClick={(e) => {mainFn(e.target.parentNode)}} className='mask div_on_click' key={props.index}>
