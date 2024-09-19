441fe61db5bd59ca47dec71073106a23cab05eb1
https://github.com/svkzdenosvk <zdenko.snurik@gmail.com>
2024-09-19 20:12:29 +0200

    to show original logic rewritten with callbacks

--- src/components/AfterGame/TimeAndStart.js
+++ src/components/AfterGame/TimeAndStart.js
@@ -1,37 +1,23 @@
-import { useEffect } from 'react';
 import { _stylingAfterStart } from '../../_inc/_inc_functions';
  
-export const TimeAndStart = ({level,shuffle,seconds,setSeconds,setIntervalSecond,color,isRunning, setIsRunning}) => {
-    
-    useEffect(() => {
-      if (!isRunning) return;
-
-      function _incrementSeconds() {
-        setSeconds(seconds => seconds + 1);
-      }
-      // set interval to increase seconds
-      const secondInterval = setInterval(_incrementSeconds, 1000);
-      setIntervalSecond(secondInterval);
-    
-      // Cleaning the interval when unmounting or changing dependencies
-      // return () => {
-      //   clearInterval(secondInterval);
-      // };
-    }, [isRunning, setSeconds, setIntervalSecond]);
-   
-
+export const TimeAndStart = ({level,shuffle,seconds,setSeconds,setIntervalSecond,color}) => {
+      
+    function _incrementSeconds(){/*---------------------------------------------------partial f. for change seconds number by increment +1*/
+      // setSeconds(seconds=seconds+1);
+      setSeconds(prevSeconds => prevSeconds + 1);
+  }
+  
     function timer(){/*---------------------------------------------------------------button start */
      
       _stylingAfterStart();
-
-      setIsRunning(true);
-
-      // in hardest level shuffle img´s
+  
+      // increment seconds + in hardest level shuffle img´s
+      setIntervalSecond(setInterval(_incrementSeconds, 1000));
       if(level==="hardest"){/*---------------------------------------------------------working only in hardest version *//*maybe this can by removed from timer();*/
          setInterval(shuffle, 800);
       }
     }
-    
+  
     return (
       <div id="timeAndStart">
           <div style={{color}} id="seconds"  >{seconds} s</div>
